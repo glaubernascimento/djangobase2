cf144f2e682c4205ce0d33a18c790a52c8c2f83c
glaubernascimento <glaubergames121@gmail.com>
2026-04-14 19:49:39 -0300

    Ajustes até o momento

--- cadastro/urls.py
+++ cadastro/urls.py
@@ -11,5 +11,14 @@ urlpatterns = [
     path('contato/', views.contato, name='contato'),
 
     # Rota para cadastrar pessoa
-    path('adicionar/', views.adicionar, name='adicionar'),
+    path('new/', views.adicionar, name='adicionar'),
+
+    # Rota para os detalhes de uma pessoa
+    path('pessoa/<int:id>/', views.detalhe, name='detalhe'),
+
+    # Rota para editar a pessoa específica
+    path('pessoa/<int:id>/editar/', views.editar, name='editar'),
+
+    # Rota para apagar a pessoa específica (hard delete)
+    path('pessoa/<int:id>/deletar/', views.deletar, name='deletar'),
 ]
